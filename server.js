210d4fdfcc3bdadb1e5c50a89c50e29d748c7f16
Jonni Tammisto <jonni.tammisto@skhole.fi>
2019-03-04 15:57:12 +0200

    Start to add resolvers and real typeDefs

--- server.js
+++ server.js
@@ -1,47 +1,30 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
-const todos = [
-  {
-    task: 'Wash car',
-    completed: false
-  },
-  {
-    task: 'Clean room',
-    completed: false
-  }
-];
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require('./resolvers');
 
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
+require('dotenv').config({ path: 'variables.env' });
 
-  type Query {
-    getTodos: [Todo]
-  }
+const User = require('./models/User');
+const Post = require('./models/Post');
 
-  type Mutation {
-    addTodo(task: String, completed: Boolean): Todo
-  }
-`;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+.then(() => console.log('db connected'))
+.catch(err => console.error(err));
 
-const resolvers = {
-  Query: {
-    getTodos: () => todos
-  },
-  Mutation: {
-    addTodo: (_, { task, completed }) => {
-      const todo = { task, completed };
-      todos.push(todo);
-      return todo;
-    }
-  }
-};
+mongoose.set('useCreateIndex', true);
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {
